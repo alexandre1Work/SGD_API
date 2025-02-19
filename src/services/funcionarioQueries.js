b2041fa41ce5dd4e6fe4ca3efd3d5a9b0bd906ae
alexandre1Work <alexandre.velosowork@gmail.com>
2025-02-19 17:47:28 -0300

    creation of funcionarioQueries

---
+++ src/services/funcionarioQueries.js
@@ -0,0 +1,26 @@
+import { sql } from "./db";
+
+export const FuncionarioQueries = {
+    getAll: async () => {
+        return await sql`SELECT * FROM tb_funcionario`;
+    },
+    getById: async (id) => {
+        return await sql`SELECT * FROM tb_funcionario WHERE id_funcionario = ${id}`;
+    },
+    create: async ({cpf, nome, telefone, endereco, email}) => {
+        return await sql`INSERT INTO tb_funcionario (cpf, nome, telefone, endereco, email)
+        VALUES (${cpf}, ${nome}, ${telefone}, ${endereco}, ${email});`;
+    },
+    update: async (id, {cpf, nome, telefone, endereco, email}) => {
+        return await sql`UPDATE tb_funcionario
+        SET cpf = ${cpf},  
+          nome = ${nome},  
+          telefone = ${telefone},  
+          endereco = ${endereco},  
+          email = ${email}
+          WHERE id_funcionario = ${id};`;
+    },
+    delete: async (id) => {
+        return await sql`DELETE FROM tb_funcionario WHERE id_funcionario = ${id}`
+    }
+}
