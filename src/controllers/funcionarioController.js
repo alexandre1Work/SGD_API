9e2112ae671bac39c073183c3585d70b1dbd9f37
alexandre1Work <alexandre.velosowork@gmail.com>
2025-02-19 18:04:53 -0300

    creation of funcionarioController

---
+++ src/controllers/funcionarioController.js
@@ -0,0 +1,50 @@
+import { FuncionarioQueries } from "../services/funcionarioQueries";
+
+export async function getFuncionarios(req, res) {
+    try {
+        const funcionarios = await FuncionarioQueries.getAll();
+        res.json(funcionarios);
+    } catch(error) {
+        console.error(error);
+        res.status(500).send("Erro ao buscar funcionarios");
+    }
+}
+
+export async function getFuncionario(req, res) {
+    try {
+        const id = req.params.id;
+        const funcionario = await FuncionarioQueries.getById(id);
+        res.json(funcionario);
+    } catch(error) {
+        res.status(500).send("Erro ao obter funcionário");
+    }
+}
+
+export async function createFuncionario(req, res) {
+    try {
+        await FuncionarioQueries.create(req.body);
+        res.status(200).send("Funcionário criado com sucesso!");
+    } catch(error) {
+        res.status(500).send("Não foi possível criar o funcionário");
+    }
+}
+
+export async function updateFuncionario(req, res) {
+    try {
+        const id = req.params.id;
+        await FuncionarioQueries.update(id, req.body)
+        res.status(200).send("Funcionário atualizado com sucesso");
+    } catch (error) {
+        res.status(500).send("Não foi possível atualizar o funcionário");
+    }
+}
+
+export async function deleteFuncionario(req, res) {
+    try {
+        const id = req.params.id;
+        await FuncionarioQueries.delete(id);
+        res.status(200).send("Funcionário deletado com sucesso");
+    } catch (error) {
+        res.status(500).send("Erro ao deletar o funcionario");
+    }
+}   
--- src/services/funcionarioQueries.js
+++ src/services/funcionarioQueries.js
@@ -24,3 +24,4 @@ export const FuncionarioQueries = {
         return await sql`DELETE FROM tb_funcionario WHERE id_funcionario = ${id}`
     }
 }
+
