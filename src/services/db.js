82c679d20c112fbc33c54cf76a8038747b88c045
ArtmdX <arthur.mdx1234@gmail.com>
2025-02-21 12:04:47 -0300

    returning

--- src/services/db.js
+++ src/services/db.js
@@ -1,18 +1,6 @@
 import postgres from "postgres";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
-
-export const sql = postgres(connectionString, {
-  host: "db.ruhvvekthwemutwvxhgs.supabase.co",
-  port: 5432,
-  ssl: "require",
-  family: 4, // Força IPv4
-});
-
-// Teste de conexão para log
-sql`SELECT NOW()`
-  .then((res) => console.log("Conexão bem-sucedida:", res))
-  .catch((err) => console.error("Erro na conexão:", err));
+export const sql = postgres(connectionString);
