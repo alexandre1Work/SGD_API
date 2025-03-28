48a4cec2a53c78038d2785338f673f7e885c0aa8
alexandre1Work <alexandre.velosowork@gmail.com>
2025-03-28 16:14:49 -0300

    validation: checking if the user exists at login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -8,16 +8,28 @@ export async function login(req, res) {
   const { email, senha } = req.body;
 
   try {
+    // Verificar se usuário existe
     const usuario = await getUserEmail(email);
+    if (!usuario) {
+      return res.status(404).send("Usuário não encontrado.");
+    }
+
+    //comparar senha
     const senhaChecada = await bcrypt.compare(senha, usuario.senha);
     if (!senhaChecada) {
       return res.status(422).send("Senha inválida.");
     }
 
-    const token = jwt.sign({ userId: usuario.id, userName: usuario.nome, userRole: usuario.cargo }, process.env.SECRET);
+    //geração do token
+    const token = jwt.sign(
+      { userId: usuario.id, userName: usuario.nome, userRole: usuario.cargo },
+      process.env.SECRET
+    );
+
     res.status(200).json(token);
   } catch (error) {
     res.status(500).send(error);
+    res.status(500).send("Erro interno no servidor.");
   }
 }
 
